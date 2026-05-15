f538a22528bcd2a7d2f209423abf5c781f2fc389
Roberto Selbach <rselbach@rselbach.com>
2026-05-15 08:01:11 -0400

    Harden atomic JSON writes

--- internal/atomicfile/atomicfile.go
+++ internal/atomicfile/atomicfile.go
@@ -2,18 +2,31 @@ package atomicfile
 
 import (
 	"encoding/json"
+	"fmt"
 	"os"
 	"path/filepath"
 )
 
 func WriteJSON(path string, v any) error {
-	if err := os.MkdirAll(filepath.Dir(path), 0o755); err != nil {
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, 0o755); err != nil {
 		return err
 	}
 
-	tmp := path + ".tmp"
-	file, err := os.OpenFile(tmp, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0o600)
+	file, err := os.CreateTemp(dir, filepath.Base(path)+".*.tmp")
 	if err != nil {
+		return err
+	}
+	tmp := file.Name()
+	cleanup := true
+	defer func() {
+		if cleanup {
+			_ = os.Remove(tmp)
+		}
+	}()
+
+	if err := file.Chmod(0o600); err != nil {
+		_ = file.Close()
 		return err
 	}
 
@@ -23,8 +36,34 @@ func WriteJSON(path string, v any) error {
 		_ = file.Close()
 		return err
 	}
+	if err := file.Sync(); err != nil {
+		_ = file.Close()
+		return err
+	}
 	if err := file.Close(); err != nil {
 		return err
 	}
-	return os.Rename(tmp, path)
+	if err := os.Rename(tmp, path); err != nil {
+		return err
+	}
+	cleanup = false
+	return syncDir(dir)
+}
+
+func syncDir(dir string) error {
+	file, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	if err := file.Sync(); err != nil {
+		closeErr := file.Close()
+		if closeErr != nil {
+			return fmt.Errorf("sync directory %q: %w; close directory: %v", dir, err, closeErr)
+		}
+		return fmt.Errorf("sync directory %q: %w", dir, err)
+	}
+	if err := file.Close(); err != nil {
+		return err
+	}
+	return nil
 }
